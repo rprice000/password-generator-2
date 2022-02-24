a7fe726f6268a3414acc22c22d625a3bc93ecca2
rprice000 <rtprice21@gmail.com>
2022-02-24 12:57:49 -0600

    added loop logic to loop through individual arrays and display objects based on number of characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment code here
-
+var numberCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var lowerCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialCharacters = ["?", "/", ".", ">", "<", ",", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,7 +17,7 @@ function writePassword() {
     return;
   };
 
-  numberOfCharacters = parseInt(numberOfCharacters); // turns all inputs into  a number
+  numberOfCharacters = parseInt(numberOfCharacters);
   
   if (numberOfCharacters >= 8 && numberOfCharacters <= 128 ) {
 
@@ -30,65 +33,104 @@ function writePassword() {
 
       var upper = window.confirm('Click OK if you want to include Upper Case Characters.');
       console.log(upper);
-     
-    }  
 
       if ( special === true && numeric === true && lower === true && upper === true ) {
         window.alert('ALL TRUE');
+
       }
+
       else if (special === true && numeric === false && lower === false && upper === false) {
         window.alert('SPECIAL');
+        for(var i = 0; i < numberOfCharacters; i++) {
+          var randomAnswer = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+          console.log(randomAnswer);
+        }
       }
+
       else if (special === false && numeric === true && lower === false && upper === false) {
         window.alert('NUMERIC');
+        for(var i = 0; i < numberOfCharacters; i++) {
+          var randomAnswer = numberCharacters[Math.floor(Math.random() * numberCharacters.length)];
+          console.log(randomAnswer);
+        }
       }
+
       else if (special === false && numeric === false && lower === true && upper === false) {
         window.alert('LOWER');
+        for(var i = 0; i < numberOfCharacters; i++) {
+          var randomAnswer = lowerCharacters[Math.floor(Math.random() * lowerCharacters.length)];
+          console.log(randomAnswer);
+        }
       }
+
       else if (special === false && numeric === false && lower === false && upper === true) {
         window.alert('UPPER');
+        for(var i = 0; i < numberOfCharacters; i++) {
+          var randomAnswer = upperCharacters[Math.floor(Math.random() * upperCharacters.length)];
+          console.log(randomAnswer);
+        }
       }
+
       else if (special === true && numeric === true && lower === false && upper === false) {
         window.alert('SPECIAL / NUMERIC');
+
       }
       else if (special === true && numeric === false && lower === true && upper === false) {
         window.alert('SPECIAL / LOWER');
+
       }
+
       else if (special === true && numeric === false && lower === false && upper === true) {
         window.alert('SPECIAL / UPPER');
+
       }
+
       else if (special === true && numeric === false && lower === true && upper === true) {
         window.alert('SPECIAL / LOWER / UPPER');
+
       }
       else if (special === true && numeric === true && lower === false && upper === true) {
         window.alert('SPECIAL / NUMERIC / UPPER');
+
       }
+
       else if (special === true && numeric === true && lower === true && upper === false) {
         window.alert('SPECIAL / NUMERIC / LOWER');
+
       }
+
       else if (special === false && numeric === true && lower === true && upper === false) {
         window.alert('NUMERIC / LOWER');
+
       }
+
       else if (special === false && numeric === true && lower === false && upper === true) {
         window.alert('NUMERIC / UPPER');
+
       }
       else if (special === false && numeric === true && lower === true && upper === true) {
         window.alert('NUMERIC / LOWER / UPPER');
+
       }
+
       else if (special === false && numeric === false && lower === true && upper === true) {
         window.alert('LOWER / UPPER');
+        
       }
+
       else {
         window.alert('You must confirm at least one character type.');
+        return generatePassword();
       };
-  
-/*  THIS WAS GIVEN TO US */
-      var password = generatePassword();
-      var passwordText = document.querySelector("#password");
-      passwordText.value = password;
-/*  THIS WAS GIVEN TO US */
 
+    }
+    /*  THIS WAS GIVEN TO US */
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+    /*  THIS WAS GIVEN TO US */
   }
+
   else {
     window.alert('Password Length must be a Number between 8 and 128 characters.');
     return writePassword();
